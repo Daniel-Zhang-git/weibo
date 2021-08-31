b467db4c24d338af1471eebc57964a3a086c0a21
zhangwenchao <913175244@qq.com>
2021-09-01 01:35:22 +0800

    静态文件浏览器缓存问题

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,6 +10,6 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
-
+//js 方法的第二个参数可以用来自定义生成的 JS 文件的输出目录。
 mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+   .sass('resources/sass/app.scss', 'public/css').version();;
